2812c976ff56b7f41037d5a9a2890bcd091dc095
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2020-03-30 09:15:16 -0300

    Updates argument / options flow

--- Sources/Tractor/main.swift
+++ Sources/Tractor/main.swift
@@ -2,15 +2,22 @@ import TractorCore
 import ArgumentParser
 
 struct Tractor: ParsableCommand {
-    @Option() var XCReportPath: String
+    @Option() var XCReportPath: String?
+    @Argument() var report: String?
     
     func run() throws {
-        let register = TractorRegister(reportFileName: XCReportPath)
-        try register.createTestRegister()
+        if let path = XCReportPath {
+            let register = TractorRegister(reportFileName: path)
+            try register.createTestRegister()
+        } else if report != nil {
+            print("🚜 should generate report")
+        }
+        
     }
 }
 
 // Test-FlakyTestsProject-2020.03.28_19-49-19--0300 success
 // Test-FlakyTestsProject-2020.03.28_19-49-24--0300 failure
 
+// Tractor.main(["report"])
 Tractor.main(["--xc-report-path", "Test-FlakyTestsProject-2020.03.28_19-49-24--0300"])
