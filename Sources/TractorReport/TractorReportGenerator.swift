e1de14ecff9c58951ff6cf93a1bcf940268d8fd6
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2020-04-04 16:58:38 -0300

    Initial structure of report

--- Sources/TractorReport/TractorReportGenerator.swift
+++ Sources/TractorReport/TractorReportGenerator.swift
@@ -58,28 +58,47 @@ public class TractorReportGenerator {
             }
             
             print(output)
+            
+            let html = HTML(
+                .head(
+                    .title("Tractor Report"),
+                    .stylesheet("styles.css")
+                ),
+                .body(
+                    .div(
+                        .h1("Tractor Report")
+                    ),
+                    .wrapped(output.last!),
+                    .div(
+                        .p("Made with love")
+                    )
+                )
+            )
+            
+            let contentString = html.render()
+            let htmlData = contentString.data(using: .utf8)
+            
+            try? Folder.current.createFile(at: "tractor-report/report.html", contents: htmlData)
         }
         
         // load files from tractor-output folder
         // generates a array of object
         // start: 0, end: 2
-        // 
-        
-        let html = HTML(
-            .head(
-                .title("My website"),
-                .stylesheet("styles.css")
-            ),
-            .body(
-                .div(
-                    .h1("Tractor Report"),
-                    .p("Writing HTML in Swift is pretty great!")
-                )
-            )
-        )
-        
-        let content = html.render()
+        //
         
     }
     
+}
+
+extension Node where Context: HTML.BodyContext {
+    static func wrapped(_ output: TractorOutput) -> Self {
+        let tests = output.failures.map { $0 }
+        
+        return .div(
+            .h1("Failure Tests"),
+            .ul(.forEach(tests) {
+                .li("\($0.name) - \($0.target)")
+            })
+        )
+    }
 }
