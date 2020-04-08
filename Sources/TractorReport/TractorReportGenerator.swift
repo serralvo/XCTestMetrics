6e4fb52f2abed7a3b055c78ba875fce036fab68c
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2020-04-07 21:27:23 -0300

    Updates log on success

--- Sources/TractorReport/TractorReportGenerator.swift
+++ Sources/TractorReport/TractorReportGenerator.swift
@@ -2,22 +2,22 @@ import Files
 import Foundation
 import Plot
 import TractorEntity
+import TractorDisplay
 
 public class TractorReportGenerator {
-    
-    private func getDecoder() -> JSONDecoder {
-        return TractorOutput.decoder
-    }
- 
-    public init() {}
     
     public func generate() {
         let report = ListReportGenerator()
         let content = report.generate()
         let htmlData = content.data(using: .utf8)
         
-        try? Folder.current.createFile(at: "tractor-report/report.html", contents: htmlData)
-        
+        do {
+            try Folder.current.createFile(at: "tractor-report/report.html", contents: htmlData)
+            Display.success(message: "Report has been saved. Check it on tractor-report folder.")
+        } catch  {
+            Display.error(message: error.localizedDescription)
+        }
+
     }
     
 }
