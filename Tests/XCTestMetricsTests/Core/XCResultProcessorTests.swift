b4da86c9fe9437c76f6283bccabba5b31d70a94e
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2020-06-15 14:15:58 -0300

    Adds tests for XCResultProcessor

--- Tests/XCTestMetricsTests/Core/XCResultProcessorTests.swift
+++ Tests/XCTestMetricsTests/Core/XCResultProcessorTests.swift
@@ -1,20 +1,34 @@
 import Foundation
+import Files
 import XCTest
 
 @testable import Core
 
 final class XCResultProcessorTests: XCTestCase {
     
+    private lazy var sut = XCResultProcessor(resultFilePath: "filePath", shell: spy)
     private let spy = ShellExecutableProtocolSpy()
     
     func test_persistOutputFile_shouldCallShellWithCorrectParameters() {
-        
-        let sut = XCResultProcessor(resultFilePath: "filePath", shell: spy)
         
         try? sut.persistOutputFile()
         
         XCTAssertEqual(spy.runCalled, true)
         XCTAssertEqual(spy.runPassed, ["xcrun xcresulttool get --format json --path filePath > output.json"])
+    }
+    
+    func test_removeJSONFileFromXCResult_shouldRemoveFile() {
+        
+        try? Folder.current.createFile(at: "output.json")
+        
+        XCTAssertEqual(Folder.current.containsFile(at: "output.json"), true)
+        
+        do {
+            try sut.removeJSONFileFromXCResult()
+            XCTAssertEqual(Folder.current.containsFile(at: "output.json"), false)
+        } catch {
+            XCTFail("Expected success")
+        }
     }
     
 }
