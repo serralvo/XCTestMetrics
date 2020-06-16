74e8cd16253b22bafd9a4f7bd2b75c7179ad6295
Fabricio Serralvo <fabricio.serralvo@gmail.com>
2020-06-16 18:40:03 -0300

    Updates formula

--- Formula/xctestmetrics.rb
+++ Formula/xctestmetrics.rb
@@ -3,8 +3,8 @@
 class Xctestmetrics < Formula
   desc "Command-line tool that provides metrics about your project tests"
   homepage "https://github.com/serralvo/XCTestMetrics"
-  url "https://github.com/serralvo/XCTestMetrics"
-  version "0.0.5"
+  url "https://github.com/serralvo/XCTestMetrics.git", :tag => "0.0.6"
+  version "0.0.6"
   
   depends_on :xcode => ["11.0", :build]
 
